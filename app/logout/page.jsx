074fc42ef2c5b0fd0a4f5b95aa50977a0f5261fc
sakib-xrz <sakibxrz21@gmail.com>
2024-10-04 13:02:51 +0600

    Add loading animation to logout page and create Logo component

--- app/logout/page.jsx
+++ app/logout/page.jsx
@@ -2,6 +2,7 @@
 "use client";
 
 import removeAccessToken from "@/actions/removeAccessToken";
+import Logo from "@/components/icon/Logo";
 import { logout, removeUserInfo } from "@/utils/auth";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -20,5 +21,10 @@ export default function Logout() {
     logOut();
   }, []);
 
-  return null;
+  return (
+    <div className="flex h-screen animate-pulse items-center justify-center gap-2">
+      <Logo className={"size-6"} />
+      <p className="text-xl font-bold text-primary">Logging out...</p>
+    </div>
+  );
 }
