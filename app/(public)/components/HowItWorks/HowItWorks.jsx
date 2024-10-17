410adee569d0e9907881ae11dc7156733e9d4547
nahid-dev <nahidv232@gmail.com>
2024-10-18 01:37:43 +0600

    design and basic implementation complete

--- app/(public)/components/HowItWorks/HowItWorks.jsx
+++ app/(public)/components/HowItWorks/HowItWorks.jsx
@@ -1,24 +1,41 @@
+import Container from "@/components/shared/Container";
+import Image from "next/image";
 import React from "react";
 
 const HowItWorks = () => {
   return (
     <div>
-      {[...Array(10)].map((_, i) => (
-        <div key={i}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
-          recusandae sunt ratione neque culpa maiores dolorum, possimus saepe
-          sapiente accusamus eum sequi nemo ullam repellendus consectetur
-          aliquam nostrum temporibus. Vero hic odit, quasi reiciendis nam sequi
-          provident exercitationem similique eum! Accusamus aspernatur
-          consequuntur vel exercitationem enim reiciendis voluptatum ipsum dicta
-          nobis, quos soluta ea. Aspernatur libero corrupti, dolorum expedita
-          ullam repellat reiciendis velit! Mollitia cupiditate neque animi,
-          aliquid ipsum rem repellendus. Autem est beatae dolorum quos ea
-          provident iste neque nulla vero adipisci sed quibusdam, quis accusamus
-          aspernatur. Sed, ab accusamus voluptatum neque consectetur
-          consequuntur minus facere? Voluptates, veritatis maiores.
+      <Container>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div>
+            <Image
+              src="/image/how_to_work.jpg"
+              alt="hero image 1"
+              width={500}
+              height={500}
+              loading="lazy"
+              className="rounded-md"
+            />
+          </div>
+          <div>
+            <h4 className="text-3xl font-bold">
+              Easy navigation to your doctor doctor with less instruction
+            </h4>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
+            placeat est cumque eius minus incidunt, tempore repudiandae atque
+            aliquam perspiciatis voluptatibus? Ea iure ut maxime facilis ipsa
+            ipsum. Ea eum dolor velit quam quia sapiente tempora. Aliquam quos
+            facilis dolores molestiae ad quas soluta libero blanditiis hic id
+            ipsam itaque commodi nam, beatae fuga, maxime, sapiente repellat
+            aperiam ipsum fugit vitae eveniet voluptas. Laudantium sequi
+            voluptates deleniti, enim quidem ea porro mollitia at. Nostrum,
+            eius. Ad accusantium, maiores sit tenetur enim labore nobis nihil
+            autem saepe quidem rem, quibusdam, repellendus soluta! Officiis
+            ducimus possimus consequatur reprehenderit nobis ratione perferendis
+            officia!
+          </div>
         </div>
-      ))}
+      </Container>
     </div>
   );
 };
