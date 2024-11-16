80bfab2f30f79fb00881baf94c292d67f984b543
nahid-dev <nahidv232@gmail.com>
2024-11-16 17:59:19 +0600

    Optimimzed the code services section code

--- app/(public)/components/services/services.jsx
+++ app/(public)/components/services/services.jsx
@@ -8,6 +8,45 @@ import {
 import Image from "next/image";
 import React from "react";
 
+const servicesData = [
+  {
+    icon: Stethoscope,
+    title: "General Consultation",
+    description:
+      "Get expert advice on general health concerns with our trusted professionals, available anytime for your primary care needs.",
+  },
+  {
+    icon: UserCheck,
+    title: "Specialist Doctors",
+    description:
+      "Connect with specialists across various fields for expert medical consultation tailored to your specific needs.",
+  },
+  {
+    icon: ClipboardPaste,
+    title: "Online Prescription",
+    description:
+      "Receive your prescriptions directly online after consultations, making it easy to access your necessary medications.",
+  },
+  {
+    icon: SquarePlay,
+    title: "Video Calling",
+    description:
+      "Engage in real-time, face-to-face video consultations with our doctors from the comfort of your home.",
+  },
+];
+
+const ServiceItem = ({ Icon, title, description }) => (
+  <div>
+    <div className="mb-5 flex items-center gap-2">
+      <div className="rounded-full bg-gray-100 p-3 text-gray-500">
+        <Icon className="size-12" />
+      </div>
+      <h4 className="text-xl font-semibold text-gray-800">{title}</h4>
+    </div>
+    <p className="text-gray-500">{description}</p>
+  </div>
+);
+
 const Services = () => {
   return (
     <div className="bg-gray-50 py-10">
@@ -19,76 +58,36 @@ const Services = () => {
           <div className="grid grid-cols-1 gap-16 sm:grid-cols-3">
             {/* LEFT */}
             <div className="flex flex-col justify-between">
-              <div>
-                <div className="mb-5 flex items-center gap-2">
-                  <div className="rounded-full bg-gray-100 p-3 text-gray-500">
-                    <Stethoscope className="size-12" />
-                  </div>
-                  <h4 className="text-xl font-semibold text-gray-800">
-                    General Consultation
-                  </h4>
-                </div>
-                <p className="text-gray-500">
-                  Get expert advice on general health concerns with our trusted
-                  professionals, available anytime for your primary care needs.
-                </p>
-              </div>
-              <div>
-                <div className="mb-5 flex items-center gap-2">
-                  <div className="rounded-full bg-gray-100 p-3 text-gray-500">
-                    <UserCheck className="size-12" />
-                  </div>
-                  <h4 className="text-xl font-semibold text-gray-800">
-                    Specialist Doctors
-                  </h4>
-                </div>
-                <p className="text-gray-500">
-                  Connect with specialists across various fields for expert
-                  medical consultation tailored to your specific needs.
-                </p>
-              </div>
+              {servicesData.slice(0, 2).map((service, index) => (
+                <ServiceItem
+                  key={index}
+                  Icon={service.icon}
+                  title={service.title}
+                  description={service.description}
+                />
+              ))}
             </div>
             {/* MIDDLE */}
             <div className="flex flex-col justify-center">
               <Image
                 src="/image/services/service.png.webp"
                 alt="Service image"
-                width={6000}
+                width={600}
                 height={600}
                 loading="lazy"
+                className="rounded-md"
               />
             </div>
             {/* RIGHT */}
             <div className="flex flex-col justify-between">
-              <div>
-                <div className="mb-5 flex items-center gap-2">
-                  <div className="rounded-full bg-gray-100 p-3 text-gray-500">
-                    <ClipboardPaste className="size-12" />
-                  </div>
-                  <h4 className="text-xl font-semibold text-gray-800">
-                    Online Prescription
-                  </h4>
-                </div>
-                <p className="text-gray-500">
-                  Receive your prescriptions directly online after
-                  consultations, making it easy to access your necessary
-                  medications.
-                </p>
-              </div>
-              <div>
-                <div className="mb-5 flex items-center gap-2">
-                  <div className="rounded-full bg-gray-100 p-3 text-gray-500">
-                    <SquarePlay className="size-12" />
-                  </div>
-                  <h4 className="text-xl font-semibold text-gray-800">
-                    Video Calling
-                  </h4>
-                </div>
-                <p className="text-gray-500">
-                  Engage in real-time, face-to-face video consultations with our
-                  doctors from the comfort of your home.
-                </p>
-              </div>
+              {servicesData.slice(2).map((service, index) => (
+                <ServiceItem
+                  key={index}
+                  Icon={service.icon}
+                  title={service.title}
+                  description={service.description}
+                />
+              ))}
             </div>
           </div>
         </div>
